7c1d4905c10f11b91b4a0f09c1995dd61b231a80
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2025-01-28 00:10:57 +0100

    Update requirements and environment setup

--- build_env.sh
+++ build_env.sh
@@ -65,4 +65,8 @@ conda run -n ${env_name} cmake -DCOMPUTE_BACKEND=cuda -S .
 conda run -n ${env_name} make
 conda run -n ${env_name} pip install -e .
 
+cd ../..
+
+conda run -n ${env_name} conda env config vars set PYTHONPATH=${PYTHONPATH}:${PWD}/src/
+
 exit 0
