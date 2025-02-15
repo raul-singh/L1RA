5f411480094c9cfcd25ff6df8309b8ac8d90c923
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2025-02-15 18:08:39 +0100

    Small fix

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,9 +3,9 @@ set -xe
 
 # Baselines
 conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/mistral.yml
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama.yml
+conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama3.yml
 conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/mistral_alt.yml
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama_alt.yml
+conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama3_alt.yml
 # Experiments
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/mistral.yml
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/llama.yml
+conda run -n l1ra python ./src/script/train_assistant.py --config-path ./resources/configs/mistral.yml
+conda run -n l1ra python ./src/script/train_assistant.py --config-path ./resources/configs/llama3.yml
