c4bb57bc03355532bdf3ff282a404ca177d25237
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2025-02-15 18:11:08 +0100

    Disable unused experiments in script

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,8 +4,8 @@ set -xe
 # Baselines
 conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/mistral.yml
 conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama3.yml
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/mistral_alt.yml
-conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama3_alt.yml
+# conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/mistral_alt.yml
+# conda run -n l1ra_baseline python ./src/script/train_assistant.py --config-path ./resources/configs/baselines/llama3_alt.yml
 # Experiments
 conda run -n l1ra python ./src/script/train_assistant.py --config-path ./resources/configs/mistral.yml
 conda run -n l1ra python ./src/script/train_assistant.py --config-path ./resources/configs/llama3.yml
