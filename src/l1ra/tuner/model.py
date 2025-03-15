3c25fe3e57d6046f3bc5b2e4911a2cd66df32cbe
vincenzo-scotti <vincenzo.scotti@kit.edu>
2025-03-15 22:54:30 +0100

    Add temporary log

--- src/l1ra/tuner/model.py
+++ src/l1ra/tuner/model.py
@@ -269,6 +269,7 @@ class L1RAModel(LoraModel):
                     A_matrix = None
 
     def update_ranks(self, global_step, num_training_steps, num_warmup_steps):
+        print(f'Rank update: global_step: {global_step}, num_training_steps: {num_training_steps}, num_warmup_steps: {num_warmup_steps}, rank_update_ratio: {self.peft_config[self.trainable_adapter_name].rank_update_ratio}')
         if 0 <= self.peft_config[self.trainable_adapter_name].rank_update_ratio < 1:
             interval = int(self.peft_config[self.trainable_adapter_name].rank_update_ratio * num_training_steps)
         else:
